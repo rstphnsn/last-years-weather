3dcdd1051cff91dbe0007cbe7e4ea805693458a8
Richard Stephenson <rich@stphnsn.com>
2015-01-18 00:10:20 -0800

    Refactored JS to use promises and reduce repetition

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,19 +1,33 @@
 var rps = rps || {};
 rps.lyw = rps.lyw || {};
 
-rps.lyw.app = (function (window, document) {
+rps.lyw.app = (function (window, $, document) {
     'use strict';
 
-    var forecastApiKey = "80bafaf6564f9d766fc973884955088b",
+    var $window = $(window),
+        $main = $('.main'),
+        $controls = $('.controls'),
+        $tempNow = $('.temperature-today .temperature'),
+        $tempThen = $('.temperature-last-year .temperature'),
+        $tempThenHeader = $('.temperature-last-year h2'),
+        $message = $('h1'),
+
+        forecastApiKey = "80bafaf6564f9d766fc973884955088b",
         url = "https://api.forecast.io/forecast/",
-        lastYearsTemperature = null,
-        todaysTemperature = null,
-        displayUnit = 'us',
-        displayDate = 'year',
+        
+        tempNow,
+        tempThen,
+        lat,
+        lng,
+        geoLocationOptions = {
+            timeout: 5000
+        },
+        
         units = {
             'us': 'F',
             'si': 'C'
         },
+        
         dates = {
             'year': 31536000000,
             'month': 2592000000,
@@ -21,40 +35,10 @@ rps.lyw.app = (function (window, document) {
             'day': 86400000
         },
 
-    getForecastData = function (lat, lng, getLastYear) {
-        if (getLastYear) {
-            $.getJSON(url + forecastApiKey + '/' + lat + ',' + lng + ',' + getLastYear + '?units=' + displayUnit + '&callback=?', lastYearForecastSuccess);
-        } else {
-            $.getJSON(url + forecastApiKey + '/' + lat + ',' + lng + '?units=' + displayUnit + '&callback=?', currentForecastSuccess);
-        }
-    },
-
-    lastYearForecastSuccess = function (data) {
-        updateTemperature(data, true);
-    },
-
-    currentForecastSuccess = function (data) {
-        updateTemperature(data);
-    },
-
-    forecastFailure = function (err) {
-        console.log(err);
-    },
-
-    updateTemperature = function (data, getLastYear) {
-        var targetDiv,
-            temperature = Math.round(data.currently.temperature),
-            temperatureText;
-        if (getLastYear) {
-            targetDiv = $('.temperature-last-year');
-            lastYearsTemperature = temperature;
-        } else {
-            targetDiv = $('.temperature-today');
-            todaysTemperature = temperature;
-        }
-        targetDiv.find('.temperature').empty().append(temperature + '<span class="degree">˚</span><span class="unit">' + units[displayUnit] + '</span>');
-        compareTemperatures();
-    },
+        settings = {
+            'unit': 'us',
+            'date': 'year'
+        },
 
     makeDateString = function (date) {
         switch (date) {
@@ -69,116 +53,107 @@ rps.lyw.app = (function (window, document) {
         }
     },
 
-    compareTemperatures = function () {
-        var temperatureDifference,
-            temperatureMessage;
-        if (lastYearsTemperature !== null && todaysTemperature !== null) {
-            console.log('todaysTemperature = ' + todaysTemperature);
-            console.log('lastYearsTemperature = ' + lastYearsTemperature);
-            temperatureDifference = todaysTemperature - lastYearsTemperature;
-
-            if (todaysTemperature > lastYearsTemperature) {
-                temperatureMessage = 'It’s ' + temperatureDifference  + '<span class="degree">˚</span><span class="unit">' + units[displayUnit] + '</span>' + ' warmer<br>than ' + makeDateString(displayDate).toLowerCase();
-            } else if (todaysTemperature < lastYearsTemperature) {
-                temperatureMessage = 'It’s ' + Math.abs(temperatureDifference) + '<span class="degree">˚</span><span class="unit">' + units[displayUnit] + '</span>' + ' colder<br>than ' + makeDateString(displayDate).toLowerCase();
-            } else {
-                temperatureMessage = 'It’s the same temperature<br>as ' + makeDateString(displayDate).toLowerCase();
-            }
-
-            $('h1').empty().append(temperatureMessage);
-            $('.temperature-last-year h2').empty().append(makeDateString(displayDate));
-
-            lastYearsTemperature = null;
-            todaysTemperature = null;
-
-            $('.main').addClass('loaded');
-        }
-    },
-
-    addEventHandlers = function () {
-        $('.units').on('change', 'input', function () {
-            updateUnits(this);
-        });
-        $('.dates').on('change', 'input', function () {
-            updateDate(this);
-        });
-    },
-
-    updateDate = function (item) {
-        displayDate = $(item).val();
-        resetPage();
-        updateDateDisplay();
-        getLocation();
-        setUserPrefs();
-    },
-
-    updateUnits = function (item) {
-        displayUnit = $(item).val();
-        resetPage();
-        updateUnitDisplay();
-        getLocation();
-        setUserPrefs();
-    },
-
-    updateUnitDisplay = function () {
-        $('.units label').removeClass('selected');
-        $('.units label[for=display_' + displayUnit + ']').addClass('selected');
-    },
-
-    updateDateDisplay = function () {
-        $('.dates label').removeClass('selected');
-        $('.dates label[for=display_' + displayDate + ']').addClass('selected');
-    },
-
-    resetPage = function () {
-        $('.main').removeClass('loaded');
-    },
-
     getLocation = function () {
-        if (window.navigator.geolocation) {
-            window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
+        var deferred = $.Deferred();
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(deferred.resolve, deferred.reject, geoLocationOptions);
+        } else {
+            deferred.reject(new Error('Your browser does not support GeoLocation.'));
+        }
+        return deferred.promise();
+    },
+
+    compareWeather = function (weatherNow, weatherThen) {
+        var comparative,
+            message,
+            tempDiff,
+            unit = '<span class="degree">˚</span><span class="unit">' + units[settings.unit] + '</span>';
+        tempNow = Math.round(weatherNow[0].currently.temperature);
+        tempThen = Math.round(weatherThen[0].currently.temperature);
+        tempDiff = tempNow - tempThen;
+        
+        if (tempNow > tempThen) {
+            comparative = 'warmer';
+        } else if (tempNow < tempThen) {
+            comparative = 'colder';
+        }
+
+        if (!comparative) {
+            message = 'It’s the same temperature<br>as ' + makeDateString(settings.date).toLowerCase();
+        } else {
+            message = 'It’s ' + tempDiff  + unit + ' ' +  comparative + '<br>than ' + makeDateString(settings.date).toLowerCase();
+        }
+
+        // Update DOM with new data
+        $tempNow.empty().append(tempNow + unit);
+        $tempThen.empty().append(tempThen + unit);
+        $tempThenHeader.empty().append(makeDateString(settings.date));
+        $message.empty().append(message);
+
+        $main.addClass('loaded');
+    },
+
+    getWeatherNow  = function () {
+        return $.getJSON(url + forecastApiKey + '/' + lat + ',' + lng  + '?units=' + settings.unit + '&callback=?');
+    },
+
+    getWeatherThen = function (date) {
+        return $.getJSON(url + forecastApiKey + '/' + lat + ',' + lng + ',' + date + '?units=' + settings.unit + '&callback=?');
+    },
+
+    getForecast = function () {
+        $.when(getLocation()).then(function (pos) {
+            var date = new Date(pos.timestamp - dates[settings.date]);
+            date = Math.round(date.getTime() / 1000);
+            lat = pos.coords.latitude; 
+            lng = pos.coords.longitude;
+            $.when(getWeatherNow(), getWeatherThen(date)).then(compareWeather);
+        }).fail(function (err) {
+            console.error(err);
+        });
+    },
+
+    updateControls = function () {
+        $controls.find('.selected').removeClass('selected');
+        $controls.find('[value=' + settings.unit + '], [value=' + settings.date + ']').parent().addClass('selected');
+    },
+
+    retrieveSettings = function () {
+        if (window.localStorage && window.localStorage.getItem('lyw.settings.unit') && window.localStorage.getItem('lyw.settings.date')) {
+            settings.unit = window.localStorage.getItem('lyw.settings.unit');
+            settings.date = window.localStorage.getItem('lyw.settings.date');
         }
     },
 
-    locationSuccess = function (pos) {
-        var lat = pos.coords.latitude,
-            lng = pos.coords.longitude,
-            date = new Date(pos.timestamp - dates[displayDate]);
-        date = Math.round(date.getTime() / 1000);
-        getForecastData(lat, lng);
-        getForecastData(lat, lng, date);
-    },
-
-    setUserPrefs = function () {
+    storeSettings = function () {
         if (window.localStorage) {
-            window.localStorage.setItem('lyw.displayUnit', displayUnit);
-            window.localStorage.setItem('lyw.displayDate', displayDate);
+            window.localStorage.setItem('lyw.settings.unit', settings.unit);
+            window.localStorage.setItem('lyw.settings.date', settings.date);
         }
-    },
-
-    getUserPrefs = function (a, b) {
-        if (window.localStorage) {
-            if (window.localStorage.getItem('lyw.displayUnit')) {
-                displayUnit = window.localStorage.getItem('lyw.displayUnit');
-            }
-            if (window.localStorage.getItem('lyw.displayDate')) {
-                displayDate = window.localStorage.getItem('lyw.displayDate');
-            }
-        }
-    },
-
-    locationError = function (err) {
-        console.log(err);
     };
 
-    getLocation();
-    addEventHandlers();
-    getUserPrefs();
-    updateUnitDisplay();
-    updateDateDisplay();
-    $(document).bind('touchmove', function (e) {
-        e.preventDefault();
-    });
+    var init = (function () {
 
-})(window, window.document);
+        // Prevent scrolling
+        $(document).bind('touchmove', function (e) {
+            e.preventDefault();
+        });
+
+        // Set up
+        retrieveSettings();
+        updateControls();
+        getForecast();
+        
+        // Add listener
+        $controls.on('change', 'input', function () {
+            settings[$(this).attr('name')] = $(this).val();
+            $main.removeClass('loaded');
+            storeSettings();
+            updateControls();
+            getForecast();
+        });
+
+    })();
+    
+})(window, window.jQuery, window.document);
 
